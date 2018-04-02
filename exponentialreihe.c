c9fab4b21a172bfa03c8f0ffd5499b0b336f6bc6
bauerkm <37147125+bauerkm@users.noreply.github.com>
2018-04-02 22:48:11 +0200

    V2.0

--- exponentialreihe.c
+++ exponentialreihe.c
@@ -1,38 +1,55 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>			// Eigentlich nicht notwendig, da 10^(-9) auch anders geschrieben werden kann
+
 
 
 int main (int argc, char *argv[])
 {
-  float expo;
-  int i;	 					// "Genauigkeit"
-  int b = 1;
-  double Wert;
-  int fakultaet = 1;
-  unsigned int p = 1;		 			// p entspricht  
-  printf("Bitte eine Zahl eingeben\n"); 
-  scanf("%f", &expo);
-
-  for (i = 0; i <= 10; i++)
-  {	
-	while (b <= i)
-	{ 
-		
-		printf("f %i\n", fakultaet); 
-
-		Wert = (double)p / fakultaet + Wert;
-		printf("e hoch %f\nist gleich %f\n", expo, Wert); 
-		p = expo * p;
-		fakultaet = fakultaet * b;		
-		printf("p %i\nb = %i\ni = %i\n", p,b,i); 
-		b++;
 	
-	}
+	float Eingabe = 0;
+	long double Wert = 0;
+	long double fakultaet = 1;
+	long double Differenz = 10;
+	long double Subtrahend = 0;
+	long double Potenz = 1;
+	int Zaehler;
+
+
+	printf("Bitte eine Zahl eingeben.\n"); 
+	scanf("%f", &Eingabe);
+
+	printf("Ihre eingegebene Zahl lautet %f.\n",Eingabe);	
+	
+	for (Zaehler = 1; Zaehler <= 250; Zaehler++)
+ 	{	
 		
-		 
-  }	
+		if (Differenz >= pow(10, -12))
+		{ 		
+			Wert = Potenz / fakultaet + Wert;				// Summanden der Exponentialreihe
+			Potenz = Potenz * Eingabe;			
+			fakultaet = fakultaet * Zaehler;		
+			Differenz = Wert - Subtrahend;					// Differenz zwischen derzeitigem und vorherigem Summanden der Exponentialreihe
+			Subtrahend = Wert;
+			
+			if (Differenz < 0) 						// "Betrag"
+			{
+				Differenz = (-1) * Differenz; 
+			}
+		
+		}
+		else
+		{ 
+			printf("e hoch %f = %Le\nDas Ergebnis weicht maximal um 10^-12 vom exakten Wert ab.\n",Eingabe, Wert);
+			break;
+		}
+		
+		if ((Zaehler == 200) && (Differenz > pow(10, -12)))
+		{
+			printf("Ein Richtwert für e hoch %f lautet %Le\nDas exakte Ergebnis ist größer und weicht um mehr als 10^-12 vom exakten Wert ab.\n",Eingabe, Wert);
+		}
 
-  printf("e hoch %f\nist gleich %f\n", expo, Wert); 
-
+  	}	
+	
   return 0;
 }
